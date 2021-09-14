3220c35835b4e7b90326f9b0931ba280fe1eb4ad
Rahman Elsirafy <rahman2408@gmail.com>
2021-09-14 05:15:04 -0400

    commit tree 490a78d528783d124b639c1ca297ca948aa0a60d

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+function wordUp(sent) {
+  
+  sent.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1) );
+}
+
+
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map(sent => {
+
+    const words = sent.split(' ');
+    const capWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    const newSent = capWords.join(' ');
+    return newSent;
+  
+  });
 }
+// str.split(' ').forEach(wordUp => console.log(wordUp.charAt(0).toUpperCase() + wordUp.slice(1) ));
